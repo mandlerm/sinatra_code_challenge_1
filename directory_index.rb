29630d6ae26a875285c507338e86043535e22776
mandlerm <amaya.melissa@gmail.com>
2019-10-01 21:06:01 -0500

    list of docs displaying

--- directory_index.rb
+++ directory_index.rb
@@ -1,21 +1,28 @@
 require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
+require 'pathname'
 
 # [x] Pre: add files to data directory
 
-# 1. create '/' path
-# 2. pull list of files from data directory
-# 3. format data to only display file name
-# 4. Create template to display info
-# 5. erb: list
+# [x] 1. create '/' path
+# [x] 2. pull list of files from data directory
+# [x] 3. format data to only display file name
+# [x] 4. Create template to display info
+# [x] 5. erb: list
 
 # 6. route "/:filename"
 # 7. create temlate to display file
 # 8: route view to erb: #{file}
 
 get "/" do
-  @file_names = Dir.glob("data/*")
-  erb :file_display
+  files = Dir.glob("data/*")
+  @filenames = files.map { |f| Pathname.new(f).basename }
+  erb :home
+end
 
+get "/:file" do |f|
+  @file = File.read("data/#{f}")
+  @title = f.split(".")[0]
+  erb :file_display
 end
