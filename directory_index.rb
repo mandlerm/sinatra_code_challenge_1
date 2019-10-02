3a1286743d943b200f291fbd8382b9e89f01468f
mandlerm <amaya.melissa@gmail.com>
2019-10-01 19:08:37 -0500

    loaded documents for data folder

--- directory_index.rb
+++ directory_index.rb
@@ -1,3 +1,22 @@
 require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
+
+
+# [x] Pre: add files to data directory
+
+# 1. create '/' path
+# 2. pull list of files from data directory
+# 3. format data to only display file name
+# 4. Create template to display info
+# 5. erb: list
+
+# 6. route "/:filename"
+# 7. create temlate to display file
+# 8: route view to erb: #{file}
+
+
+
+get "/" do
+
+end
