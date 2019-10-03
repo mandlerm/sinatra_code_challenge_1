03391255f9c6b94ee0885139690c0282033ee9cc
mandlerm <amaya.melissa@gmail.com>
2019-10-02 19:58:09 -0500

    working and refactored

--- directory_index.rb
+++ directory_index.rb
@@ -1,35 +1,17 @@
 require "sinatra"
 require "sinatra/reloader"
 require "tilt/erubis"
-require 'pathname'
-
-# [x] Pre: add files to data directory
-
-# [x] 1. create '/' path
-# [x] 2. pull list of files from data directory
-# [x] 3. format data to only display file name
-# [x] 4. Create template to display info
-# [x] 5. erb: list
-
-# 6. route "/:filename"
-# 7. create temlate to display file
-# 8: route view to erb: #{file}
 
 get "/" do
-  files = Dir.glob("data/*")
+  # @filenames = Dir.glob("data/*").map { |f| File.basename(f, ".") }.sort
 
-  @filenames = files.map { |f| File.basename(f, ".") }.sort {|a, b| a.downcase <=> b.downcase }
+  @filenames = Dir.glob("data/*").select { |f| File.ftype(f) == "file" }.map{|file| File.basename(file)}.sort
 
-  erb :home
-end
+  if params[:sort] == 'reverse'
+    @filenames.reverse!
+    @reverse = true
+  end
 
-get "/reverse" do
-  files = Dir.glob("data/*")
-
-  @filenames = files.map { |f| File.basename(f, ".") }.sort {|a, b| a.downcase <=> b.downcase }
-
-  @filenames.reverse!
-  @reverse = true
   erb :home
 end
 
